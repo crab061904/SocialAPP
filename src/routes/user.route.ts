719ade038f11110162ef5c0886644dcf65f6e1f7
crab061904 <carlbrandws@gmail.com>
2025-05-11 01:33:09 +0800

    added get user by id

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -13,5 +13,6 @@ router.get('/:email', authenticateJWT, UserController.getUserByEmail); // Get us
 router.post('/create', UserController.createUser);  // Create a new user (open route)
 router.delete('/:id', authenticateJWT, UserController.deleteUser); // Delete user by ID (protected route)
 router.put('/:id', authenticateJWT, UserController.updateUser); // Update user by ID (protected route)
+router.get("/:id", UserController.getUserById);
 
 export default router;
